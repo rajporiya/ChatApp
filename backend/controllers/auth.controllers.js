f08480b6021cf66d0f8bfa9d3c68eb6b8e96d279
rajporiya <rajpriya81@gmail.com>
2026-04-23 07:35:03 +0530

    otp config

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,45 @@
-// step -1 send otp
+const User = require("../models/user.model");
+const otpGenerate = require("../utills/otpGenerator");
+const responce = require("../utills/responseHandler");
 
+
+// step -1 send otp
 const sendOtp = async(req,res)=>{
     const {phoneNumber, phoneSuffix, email} = req.body;
 
-    const otp =;
+    const otp =otpGenerate();
+    const expiry = new Date(Date.now() + 5*60*1000)
+    let user;
+
+    try {
+        // otp send in email
+        if(email){
+            user = await User.findOne({email});
+
+            if(!user){
+                user = new User({email})
+            }
+            user.emailOtp = otp
+            user.emailOtpExpire = expiry;
+            await user.save()
+
+            return responce(res, 200, 'Otp send to your email', {email})
+        }
+        if(!phoneNumber|| !phoneSuffix){
+            return res(res, 400, 'Phone number and phone country are requires')
+        }
+
+        const fullPhoneNumber = `${phoneSuffix} ${phoneNumber}`
+        user = await User.findOne({phoneNumber});
+        if(!user){
+            user = await new User ({phoneNumber, phoneSuffix})
+        }
+        await user.save();
+
+        return responce(res, 200, 'Oopt send successfully', user)
+    } catch (error) {
+        console.log(error);
+        return responce(res, 500,'Internal server error')
+        
+    }
 }
---
+++ backend/services/twiiloService.js
@@ -0,0 +1,51 @@
+const twilio = require("twilio");
+
+// twilio credencial from env
+const accountId = process.env.TWILLO_ACCOUNT_SID;
+const authToken = process.env.TWILLO_AUTH_TOKEN;
+const serviceSid = process.env.TWILLO_SERVICE_SID;
+
+const client = twilio(accountId, authToken);
+
+// send otp to number
+const sendOtp = async (phoneNumber) => {
+  try {
+    console.log("sending otp in number");
+    if (!phoneNumber) {
+      throw new Error("phone number is required");
+    }
+    const responce = await client.verify.v2
+      .services(serviceSid)
+      .verifications.create({
+        to: phoneNumber,
+        channel: "sms",
+      });
+    console.log("this is my otp responce");
+    return responce;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send otp");
+  }
+};
+
+// verify otp
+const verifyOtp = async (phoneNumber, otp) => {
+  try {
+    const responce = await client.verify.v2
+      .services(serviceSid)
+      .verificationChecks.create({
+        to: phoneNumber,
+        code: otp,
+      });
+    console.log("this is my otp responce", responce);
+    return responce;
+  } catch (error) {
+    console.log(error);
+    throw new Error("otp verification failed");
+  }
+};
+
+module.exports =  {
+    sendOtp,
+    verifyOtp
+}
